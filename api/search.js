c11f9c5d8683c0b5865fd3b7c60a8c066657519b
tenner3569 <77156804+tenner3569@users.noreply.github.com>
2026-07-29 13:23:57 +0900

    Update search.js

--- api/search.js
+++ api/search.js
@@ -14,6 +14,10 @@ export default async function handler(req, res) {
     );
     const rakutenData = await rakutenRes.json();
 
+    if (!rakutenData.Items || rakutenData.Items.length === 0) {
+      return res.status(200).json({ items: [] });
+    }
+
     const items = rakutenData.Items.map(({ Item: i }) => ({
       platform: 'rakuten',
       shopName: i.shopName,
